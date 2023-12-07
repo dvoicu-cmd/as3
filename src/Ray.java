058e904bf38f9bebf87e7c10f45707c7b0f9ef7c
dvoicu <dvoicu00@gmail.com>
2023-12-07 18:31:01 -0500

    Reorganized files

---
+++ src/Ray.java
@@ -0,0 +1,5 @@
+import java.util.Vector
+
+class Ray {
+
+}
---
+++ src/Raytracer.java
@@ -0,0 +1,8 @@
+import java.util.Vector;
+
+class Raytracer {
+    public static void main(String[]args) {
+        //This is gonna be the main function
+        System.out.println("Hello");
+    }
+}
