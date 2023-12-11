16df8af76d3cb9ddc7d7614cbdba7e594b7d3b5b
dvoicu <dvoicu00@gmail.com>
2023-12-10 22:27:58 -0500

    Implimented, not tested, inverse & constructor from 2d float array

--- src/MV/Matrix.java
+++ src/MV/Matrix.java
@@ -12,6 +12,19 @@ class Matrix{
         this.numCol = numCol;
     }
 
+    //Deep copy constructor
+    public Matrix(float[][] array){
+        this.numRow = array.length;
+        this.numCol = array[0].length;
+        this.mat = new float[numRow][numCol];
+
+        for(int i = 0; i<this.numRow; i++){
+            for(int j = 0; j<this.numCol; j++){
+                this.mat[i][j] = array[i][j];
+            }
+        }
+    }
+
     public float getValueAt(int row, int col){
         return this.mat[row][col];
     }
@@ -94,11 +107,66 @@ class Matrix{
 
     //Uhhhhhhhh.
     public Matrix invert4x4(){
-        return null;
-    }
+        if(this.getNumCol() != 4 || this.getNumRow() != 4){
+            return null;
+        }
+        else{
+            float[][] m = this.mat;
+            float SubFactor00 = m[2][2] * m[3][3] - m[3][2] * m[2][3];
+            float SubFactor01 = m[2][1] * m[3][3] - m[3][1] * m[2][3];
+            float SubFactor02 = m[2][1] * m[3][2] - m[3][1] * m[2][2];
+            float SubFactor03 = m[2][0] * m[3][3] - m[3][0] * m[2][3];
+            float SubFactor04 = m[2][0] * m[3][2] - m[3][0] * m[2][2];
+            float SubFactor05 = m[2][0] * m[3][1] - m[3][0] * m[2][1];
+            float SubFactor06 = m[1][2] * m[3][3] - m[3][2] * m[1][3];
+            float SubFactor07 = m[1][1] * m[3][3] - m[3][1] * m[1][3];
+            float SubFactor08 = m[1][1] * m[3][2] - m[3][1] * m[1][2];
+            float SubFactor09 = m[1][0] * m[3][3] - m[3][0] * m[1][3];
+            float SubFactor10 = m[1][0] * m[3][2] - m[3][0] * m[1][2];
+            float SubFactor11 = m[1][1] * m[3][3] - m[3][1] * m[1][3];
+            float SubFactor12 = m[1][0] * m[3][1] - m[3][0] * m[1][1];
+            float SubFactor13 = m[1][2] * m[2][3] - m[2][2] * m[1][3];
+            float SubFactor14 = m[1][1] * m[2][3] - m[2][1] * m[1][3];
+            float SubFactor15 = m[1][1] * m[2][2] - m[2][1] * m[1][2];
+            float SubFactor16 = m[1][0] * m[2][3] - m[2][0] * m[1][3];
+            float SubFactor17 = m[1][0] * m[2][2] - m[2][0] * m[1][2];
+            float SubFactor18 = m[1][0] * m[2][1] - m[2][0] * m[1][1];
 
-    public Matrix invert3x3(){
-        return null;
+            float[][] Inverse = new float[4][4];
+
+            Inverse[0][0] = (m[1][1] * SubFactor00 - m[1][2] * SubFactor01 + m[1][3] * SubFactor02);
+            Inverse[0][1] = - (m[1][0] * SubFactor00 - m[1][2] * SubFactor03 + m[1][3] * SubFactor04);
+            Inverse[0][2] =  (m[1][0] * SubFactor01 - m[1][1] * SubFactor03 + m[1][3] * SubFactor05);
+            Inverse[0][3] =  - (m[1][0] * SubFactor02 - m[1][1] * SubFactor04 + m[1][2] * SubFactor05);
+
+            Inverse[1][0] =  - (m[0][1] * SubFactor00 - m[0][2] * SubFactor01 + m[0][3] * SubFactor02);
+            Inverse[1][1] = (m[0][0] * SubFactor00 - m[0][2] * SubFactor03 + m[0][3] * SubFactor04);
+            Inverse[1][2] =  - (m[0][0] * SubFactor01 - m[0][1] * SubFactor03 + m[0][3] * SubFactor05);
+            Inverse[1][3] =  (m[0][0] * SubFactor02 - m[0][1] * SubFactor04 + m[0][2] * SubFactor05);
+
+            Inverse[2][0] =  (m[0][1] * SubFactor06 - m[0][2] * SubFactor07 + m[0][3] * SubFactor08);
+            Inverse[2][1] =  - (m[0][0] * SubFactor06 - m[0][2] * SubFactor09 + m[0][3] * SubFactor10);
+            Inverse[2][2] = (m[0][0] * SubFactor11 - m[0][1] * SubFactor09 + m[0][3] * SubFactor12);
+            Inverse[2][3] =  - (m[0][0] * SubFactor08 - m[0][1] * SubFactor10 + m[0][2] * SubFactor12);
+
+            Inverse[3][0] =  - (m[0][1] * SubFactor13 - m[0][2] * SubFactor14 + m[0][3] * SubFactor15);
+            Inverse[3][1] = (m[0][0] * SubFactor13 - m[0][2] * SubFactor16 + m[0][3] * SubFactor17);
+            Inverse[3][2] =  - (m[0][0] * SubFactor14 - m[0][1] * SubFactor16 + m[0][3] * SubFactor18);
+            Inverse[3][3] = (m[0][0] * SubFactor15 - m[0][1] * SubFactor17 + m[0][2] * SubFactor18);
+
+            float determinant = m[0][0] * Inverse[0][0] + m[0][1] * Inverse[0][1] + m[0][2] * Inverse[0][2] + m[0][3] * Inverse[0][3];
+
+            float iDeterminant = (float) (1.0/determinant); //Hopefully the casting does not mess up the solution
+            for(int i = 0; i<4; i++){
+                for(int j = 0; j<4; j++){
+                    Inverse[i][j] = Inverse[i][j]*iDeterminant;
+                }
+            }
+
+            //Now load into matrix object and export address to obj.
+            Matrix inverseOutput = new Matrix(Inverse);
+            return inverseOutput;
+        }
     }
 
 
